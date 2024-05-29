ac471759f45afa9419878f6b5235ec76f3315121
WillWSmith <will.w.smith94@gmail.com>
2024-05-29 18:19:22 -0400

    minor route adjustment

--- frontend/src/Components/Navigation/Navigation.js
+++ frontend/src/Components/Navigation/Navigation.js
@@ -7,7 +7,7 @@ const Navigation = () => {
       <h1>BOOK HUNTER</h1>
       <ul>
         <li><Link to="/allshelters">Locate A Shelter</Link></li>
-        <li><Link to="/allshelters">Add A Shelter</Link></li>
+        <li><Link to="/addshelter">Add A Shelter</Link></li>
         <li><Link to="/addbook">Add a Book</Link></li>
       </ul>
     </main>
