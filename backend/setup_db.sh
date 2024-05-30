e8c3adc61b0e8122e5a8e03e4cee5e088e250300
WillWSmith <will.w.smith94@gmail.com>
2024-05-30 14:55:06 -0400

    Setup script for db setup/reset

---
+++ backend/setup_db.sh
@@ -0,0 +1,13 @@
+# Load environment variables from the root .env file
+set -o allexport; . ../.env; set +o allexport
+
+# Create the database if it doesn't exist
+PGPASSWORD=$DB_PASSWORD psql -U $DB_USER -h $DB_HOST -c "CREATE DATABASE \"$DB_DATABASE\";" || true
+
+# Run schema files
+PGPASSWORD=$DB_PASSWORD psql -U $DB_USER -h $DB_HOST -d $DB_DATABASE -f db/schema/shelters.sql
+PGPASSWORD=$DB_PASSWORD psql -U $DB_USER -h $DB_HOST -d $DB_DATABASE -f db/schema/books.sql
+
+# Run seed files
+PGPASSWORD=$DB_PASSWORD psql -U $DB_USER -h $DB_HOST -d $DB_DATABASE -f db/seeds/shelters.sql
+PGPASSWORD=$DB_PASSWORD psql -U $DB_USER -h $DB_HOST -d $DB_DATABASE -f db/seeds/books.sql
