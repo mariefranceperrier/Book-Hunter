93e851d01c77abdd446101a7089850955086c11d
WillWSmith <will.w.smith94@gmail.com>
2024-06-04 19:44:50 -0400

    Update schema/seeds for geometry data point

--- backend/db/schema/shelters.sql
+++ backend/db/schema/shelters.sql
@@ -1,9 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS postgis;
 DROP TABLE IF EXISTS shelters CASCADE;
 CREATE TABLE shelters (
     id SERIAL PRIMARY KEY,
     civic_number INTEGER NOT NULL,
     street_name VARCHAR(50) NOT NULL,
     city VARCHAR(50) NOT NULL,
-    pin_coord POINT,
+    pin_coord GEOMETRY(Point, 4326),
     picture BYTEA
 );
