ebf86ad1bda5aed3289572135647e3ae1243bef1
WillWSmith <will.w.smith94@gmail.com>
2024-06-12 18:20:32 -0400

    Navigation style overhaul

--- frontend/src/Components/Navigation/Navigation.js
+++ frontend/src/Components/Navigation/Navigation.js
@@ -1,17 +1,42 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import './Navigation.css';
+// import logo from '../Navigation/assets/bookhunter.png';
+
+// const Navigation = () => {
+//   return (
+//     <main className="nav-bar">
+//       <Link to="/" className="header-link">
+//         <img src={logo} alt="Book Hunter Logo" className="logo" />
+//       </Link>
+//       <section className="nav-links">
+//         <div className="nav-item"><Link to="/allshelters">Locate a Shelter</Link></div>
+//         <div className="nav-item"><Link to="/addshelter">Add a Shelter</Link></div>
+//         <div className="nav-item"><Link to="/addbook">Add a Book</Link></div>
+//       </section>
+//     </main>
+//   );
+// }
+
+// export default Navigation;
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
+import logo from '../Navigation/assets/bookhunter.png';
 
 const Navigation = () => {
   return (
-    <main className="nav-bar">
-      <Link to="/" className="header-link"><h1>📚 BOOK HUNTER 📚</h1></Link>
+    <nav className="nav-bar">
+      <Link to="/" className="header-link">
+        <img src={logo} alt="Book Hunter Logo" className="logo" />
+      </Link>
       <section className="nav-links">
         <div className="nav-item"><Link to="/allshelters">Locate a Shelter</Link></div>
         <div className="nav-item"><Link to="/addshelter">Add a Shelter</Link></div>
         <div className="nav-item"><Link to="/addbook">Add a Book</Link></div>
       </section>
-    </main>
+    </nav>
   );
 }
 
